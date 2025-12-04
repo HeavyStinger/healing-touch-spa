7e96499848b3ec5f6aa2314f402e24471fa1ceab
HeavyStinger <corbinpachecocaye@gmail.com>
2025-12-04 17:56:50 -0600

    Added about us page and made navbar work correctly on desktop

--- js/main.js
+++ js/main.js
@@ -5,3 +5,24 @@ const navLinks = document.querySelector(".nav-links");
 menuButton.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+
+// Show team member information on title click
+
+const titles = document.querySelectorAll(".team .content .card .details .title");
+titles.forEach(title => {
+    title.addEventListener("click", () => {
+        const details = title.parentElement;
+        const card = details.parentElement;
+        const checker = details.classList.toggle("active");
+
+        if (checker) {
+            setTimeout(() => {
+                card.classList.toggle("active");
+            }, 400);
+        }
+        else {
+            card.classList.toggle("active");
+        }
+        console.log("Work");
+    })
+});
